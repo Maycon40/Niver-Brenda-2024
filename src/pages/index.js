bc87be51bcf28a62dd9b12bfbe2dc962d455dcea
Maycon40 <71737549+Maycon40@users.noreply.github.com>
2024-12-27 16:46:38 +0000

    Atualização página principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,13 +13,13 @@ export default function Home() {
           margin: 0;
           padding: 0;
           height: 100vh;
-          width: 100%;
+          width: 100vw;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
           background-image: url('/brendinha.jpg');
-          background-size: auto 100%;
+          background-size: contain;
           background-position: center;
           background-repeat: no-repeat;
           position: relative;
